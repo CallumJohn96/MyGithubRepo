3df819f4844a33a944f1d433484b2322ffa4bf39
Callum <ward.john.callum@gmail.com>
2022-07-20 11:51:58 +1000

    added logs

--- welcome.js
+++ welcome.js
@@ -1,4 +1,7 @@
 console.log("Hello World");
+console.log("This is my first project");
+console.log("Thanks Shane for the demo last module");
+console.log("Git isn't too hard I like it");
 
 let num1=23;
 let num2=24;
